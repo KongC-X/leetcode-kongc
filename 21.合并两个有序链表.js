d7c52f7bb29790aa678b21bcaca7a560aeefd465
zhangyiming <931993416@qq.com>
2020-12-31 12:11:13 +0800

    21

---
+++ 21.合并两个有序链表.js
@@ -0,0 +1,23 @@
+export default (l1,l2) => {
+  const res = new ListNode(0)
+  let p = res
+  let p1 = l1
+  let p2 = l2
+  while(p1 && p2){
+    if(p1.val < p2.val){
+      p.next = p1
+      p1 = p1.next
+    }else{
+      p.next = p2
+      p2 = p2.next
+    }
+    p = p.next
+  }
+  if(p1){
+    p.next = p1
+  }
+  if(p2){
+    p.next = p2
+  }
+  return res.next
+}
